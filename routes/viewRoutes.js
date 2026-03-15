4c47be572d999ee70ff1949765e74dc736d9a954
viknesh-sathya <iamviknesh.sathya@gmail.com>
2026-03-15 17:33:20 +0530

    removed the old createBookingCheckout route inside viewRoutes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -6,12 +6,7 @@ const bookingController = require('./../controllers/booking-controller');
 const router = express.Router();
 
 router.use(viewsController.alerts);
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewsController.getOverview,
-);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
